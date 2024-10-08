1694c4c9f7d9d30f0f08940b7cf72cb1fbd55a17
Dev-PeterWachira <wmainap@gmail.com>
2024-10-08 12:00:26 +0300

    to do list app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+const taskForm = document.getElementById("create-task-form");
+const taskInput = document.getElementById("new-task-description");
+const taskList = document.getElementById("tasks");
 });
+
+const createTask = (event) => {
+  event.preventDefault();
+  const taskDescription = taskInput.value;
+  if (taskDescription === ""){
+    alert("kindly enter a value");
+    return;
+
+  }
+  const newTask = document.createElement("li");
+  newTask.textContent = taskDescription;
+  const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete"; 
+    deleteButton.addEventListener("click", () => {
+      taskList.removeChild(newTask); 
+    });
+    newTask.appendChild(deleteButton); 
+    taskList.appendChild(newTask); 
+    taskInput.value = ""; 
+  };
+  taskForm.addEventListener("submit", createTask);
+});
+
